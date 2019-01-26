31eb58ba2e861a06433d216fdd2587aaf4b7cf94
Omar sleam <ooomar92@gmail.com>
2019-01-27 00:33:50 +0200

    lowQualityVersion

---
+++ Assets/CameraFOV/CameraTrackedOBJ.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTrackedOBJ : MonoBehaviour
+{
+    public GameObjectsListVariable CameraTrackedObjects;
+
+    private void OnEnable()
+    {
+        CameraTrackedObjects.Add(this.gameObject);
+    }
+    private void OnDisable()
+    {
+        CameraTrackedObjects.Remove(this.gameObject);
+    }
+}
