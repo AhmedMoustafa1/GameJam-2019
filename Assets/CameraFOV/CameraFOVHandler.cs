6c78a389a7809aee9b269b63d16388015672c43d
Omar sleam <ooomar92@gmail.com>
2019-01-27 01:10:31 +0200

    more acurate version

--- Assets/CameraFOV/CameraFOVHandler.cs
+++ Assets/CameraFOV/CameraFOVHandler.cs
@@ -15,7 +15,6 @@ public class CameraFOVHandler : MonoBehaviour
     public bool debug;
 
     GameObject ob1;
-    GameObject ob2;
     float maxDistance;
     Vector3 cameraTragetPos;
     float cameraTragetFOV;
@@ -27,75 +26,80 @@ public class CameraFOVHandler : MonoBehaviour
         cam = GetComponent<Camera>();
     }
 
-
+    private void Start()
+    {
+        LookAtPos = FindCenterPoint(trackedPlayers.list.ToArray());
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if ((maxDistance = MaxDistance(trackedPlayers.list.ToArray(), ref ob1, ref ob2)) > 0)
-        {
-            cameraTragetPos = Vector3.Lerp(ob1.transform.position, ob2.transform.position, .5f);
-        }
-        else if (trackedPlayers.list.Count == 1)
-        {
-            cameraTragetPos = trackedPlayers.list[0].transform.position;
-        }
-        else
-        {
-            return;
-        }
 
-        LookAtPos = Vector3.Lerp(LookAtPos, cameraTragetPos, Time.deltaTime * moveSpeedFactor);
-        transform.LookAt(LookAtPos);
-        if (debug)
+        if (trackedPlayers.list.Count > 0)
         {
-            Debug.DrawLine(transform.position, LookAtPos, Color.yellow);
-            Debug.DrawLine(transform.position, cameraTragetPos, Color.green);
-        }
-
-        cameraTragetFOV = (maxDistance * adjustFOVFactor);
-        if (cameraTragetFOV < minFOV) cameraTragetFOV = minFOV;
-        else if (cameraTragetFOV > MaxFOV) cameraTragetFOV = MaxFOV;
-        if (cam.orthographic)
-        {
-            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, cameraTragetFOV, Time.deltaTime * zoomSpeedFactor);
-        }
-        else
-        {
-            cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, cameraTragetFOV, Time.deltaTime * zoomSpeedFactor);
-        }
-
-        if (debug)
-        {
-            for (int i = 0; i < trackedPlayers.list.Count; i++)
+            cameraTragetPos = FindCenterPoint(trackedPlayers.list.ToArray());
+            LookAtPos = Vector3.Lerp(LookAtPos, cameraTragetPos, Time.deltaTime * moveSpeedFactor);
+            transform.LookAt(LookAtPos);
+            if (debug)
             {
-                Debug.DrawLine(trackedPlayers.list[i].transform.position, cameraTragetPos, Color.cyan);
+                Debug.DrawLine(transform.position, LookAtPos, Color.yellow);
+                Debug.DrawLine(transform.position, cameraTragetPos, Color.green);
+                for (int i = 0; i < trackedPlayers.list.Count; i++)
+                {
+                    Debug.DrawLine(trackedPlayers.list[i].transform.position, cameraTragetPos, Color.cyan);
+                }
             }
+
+            maxDistance = GetMaxDistance(trackedPlayers.list.ToArray(), cameraTragetPos, ref ob1);
+            cameraTragetFOV = (maxDistance * adjustFOVFactor);
+            if (cameraTragetFOV < minFOV) cameraTragetFOV = minFOV;
+            else if (cameraTragetFOV > MaxFOV) cameraTragetFOV = MaxFOV;
+            if (cam.orthographic)
+            {
+                cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, cameraTragetFOV, Time.deltaTime * zoomSpeedFactor);
+            }
+            else
+            {
+                cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, cameraTragetFOV, Time.deltaTime * zoomSpeedFactor);
+            }
+
         }
+
+
+
+
+
+
+
     }
 
 
-    float MaxDistance(GameObject[] objects, ref GameObject object1, ref GameObject object2)
+    float GetMaxDistance(GameObject[] objects, Vector3 centerPoint, ref GameObject fartherObj)
     {
         float maxDistance = -1f;
         float tempDestance = 0;
         if (objects.Length > 1)
         {
-            for (int x = 0; x < objects.Length - 1; x++)
+            for (int i = 0; i < objects.Length; i++)
             {
-                for (int y = x + 1; y < objects.Length; y++)
+                tempDestance = Vector3.Distance(centerPoint, objects[i].transform.position);
+                if (tempDestance > maxDistance)
                 {
-                    if (maxDistance < (tempDestance = Vector3.Distance(objects[x].transform.position, objects[y].transform.position)))
-                    {
-                        maxDistance = tempDestance;
-                        object1 = objects[x];
-                        object2 = objects[y];
-                    }
+                    maxDistance = tempDestance;
+                    fartherObj = objects[i];
                 }
             }
         }
+        return maxDistance;
+    }
 
-        return (maxDistance);
-
+    Vector3 FindCenterPoint(GameObject[] gos)
+    {
+        Vector3 center = new Vector3(0, 0, 0);
+        for (int i = 0; i < gos.Length; i++)
+        {
+            center += gos[i].transform.position;
+        }
+        return center / gos.Length;
     }
 }
