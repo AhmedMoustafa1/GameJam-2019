fd423394eeacf9968b94c55e1ffe1de780833f4c
ahmedAljazayeerly <AhmedAljazayeerly@gmail.com>
2019-01-26 23:28:33 +0200

    adding characters and doors

---
+++ Assets/CharacterControllerPoly.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class CharacterControllerPoly : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float movingForce = 5;
+    public float rotatingTime = 2;
+    bool movingH;
+    bool movingV;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            this.transform.forward = Vector3.Lerp(this.transform.forward,Vector3.forward, rotatingTime);
+            movingH = true;
+            rb.AddForce(Vector3.right * movingForce);
+        }
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            this.transform.forward = Vector3.Lerp(this.transform.forward, -Vector3.forward, rotatingTime);
+               
+
+            movingH = true;
+            rb.AddForce(-Vector3.right * movingForce);
+        }
+      
+
+        if (Input.GetAxis("Vertical") > 0 && !movingH)
+        {
+            this.transform.right = Vector3.Lerp(this.transform.right, Vector3.forward, rotatingTime);
+
+
+            rb.AddForce(Vector3.forward * movingForce);
+            movingV = true;
+        }
+
+        if (Input.GetAxis("Vertical") < 0 && !movingH)
+        {
+            this.transform.right = Vector3.Lerp(this.transform.right, -Vector3.forward, rotatingTime);
+
+            rb.AddForce(-Vector3.forward * movingForce);
+            movingV = true;
+        }
+
+
+        if (Input.GetAxis("Vertical") == 0)
+        {
+            movingV = false;
+        }
+        if (Input.GetAxis("Horizontal") == 0)
+        {
+            movingH = false;
+        }
+    }
+}
